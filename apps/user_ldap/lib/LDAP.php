7b7403134982319a0b4ea3f0df5bd373d3c72b45
Arthur Schiwon <blizzz@arthur-schiwon.de>
2021-04-26 18:04:46 +0200

    removes PagedResults adapter for PHP < 7.3
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- apps/user_ldap/lib/LDAP.php
+++ apps/user_ldap/lib/LDAP.php
@@ -47,11 +47,7 @@ class LDAP implements ILDAPWrapper {
 	protected $pagedResultsAdapter;
 
 	public function __construct() {
-		if (version_compare(PHP_VERSION, '7.3', '<') === true) {
-			$this->pagedResultsAdapter = new Php54();
-		} else {
-			$this->pagedResultsAdapter = new Php73();
-		}
+		$this->pagedResultsAdapter = new Php73();
 	}
 
 	/**
